3b539e7e467eac85b57b888fd229dcf7d14d2cec
Mohamad Homsi <homsimohamad4@gmail.com>
2023-12-15 12:37:05 +0200

    ...

--- src/Components/Item/Navbar.jsx
+++ src/Components/Item/Navbar.jsx
@@ -5,39 +5,47 @@ import propTypes from "prop-types";
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar">
-      <div className="left-navbar">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact Us</Link>
+    <div>
+      <div className="top-navbar">
+        <p>10% Off on your first order! Code: Lze2a10</p>
       </div>
-      <div className="center-navbar ">
-        <Link to="/">
-          <img src="./img/lze2a-logo.png" alt="Lze2a Logo" />
-        </Link>
-      </div>
-      <div className="right-navbar">
-        <Link to="/shop">Shop</Link>
-        <Link to="/customize">Customize</Link>
-        <span className="icon">
-          <Icon icon="material-symbols:search" />
-        </span>
-        <span
-          className="icon"
-          onClick={() => {
-            props.openRegisterModal();
-          }}
-        >
-          <Icon icon="iconamoon:profile-fill" />
-        </span>
-        <span className="icon">
-          <Icon icon="clarity:heart-line" />
-        </span>
-        <span className="icon">
-          <Icon icon="material-symbols:shopping-bag-outline" />
-        </span>
-      </div>
-    </nav>
+      <nav className="navbar">
+        <div className="left-navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
+        </div>
+        <div className="center-navbar ">
+          <Link to="/">
+            <img src="/logo.png" alt="Lze2a Logo" />
+          </Link>
+        </div>
+        <div className="right-navbar">
+          <div className="links">
+            <Link to="/shop">Shop</Link>
+            <Link to="/customize">Customize</Link>
+          </div>
+
+          <span className="icon">
+            <Icon icon="material-symbols:search" />
+          </span>
+          <span
+            className="icon"
+            onClick={() => {
+              props.openRegisterModal();
+            }}
+          >
+            <Icon icon="iconamoon:profile-fill" />
+          </span>
+          <span className="icon">
+            <Icon icon="clarity:heart-line" />
+          </span>
+          <span className="icon">
+            <Icon icon="material-symbols:shopping-bag-outline" />
+          </span>
+        </div>
+      </nav>
+    </div>
   );
 };
 
