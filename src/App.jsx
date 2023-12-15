e74feeab77984d074955a7fe0b54831469f4e72f
Mohammad Kurjieh <mohammad.kurjieh@gmail.com>
2023-12-15 08:24:49 +0200

    fix: :bug: fix wrong navigation on home screen

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
 
     return (
         <div>
-            <AboutUs></AboutUs>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" exact element={<Shop/>} />
