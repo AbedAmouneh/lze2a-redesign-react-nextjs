fb878bf2d8ffb155177b6df6ebc46395cd4e2162
AbedAmouneh <amounehabed@gmail.com>
2023-12-15 11:14:22 +0200

    Navbar

---
+++ src/Components/Item/Navbar.jsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import './Navbar.scss'; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="left-navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact Us</Link>
+      </div>
+      <div className="center-navbar ">
+        <Link to="/">
+          <img src="./img/lze2a-logo.png" alt="Lze2a Logo" />
+        </Link>
+      </div>
+      <div className="right-navbar">
+        <Link to="/shop">Shop</Link>
+        <Link to="/customize">Customize</Link>
+        <span className="icon"><Icon icon="material-symbols:search" /></span>
+        <span className="icon"><Icon icon="iconamoon:profile-fill" /></span>
+        <span className="icon"><Icon icon="clarity:heart-line" /></span>
+        <span className="icon"><Icon icon="material-symbols:shopping-bag-outline" /></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
